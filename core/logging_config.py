5dd24122fa2c34fd6fc273090d4093e18fba04fd
Kaynnandeson <kaynnandesonnascimento@gmail.com>
2026-09-19 14:03:07 -0300

    Criando a estrutura e arquivos inicias do projeto.

---
+++ core/logging_config.py
@@ -0,0 +1,18 @@
+import logging
+import logging.config
+from pathlib import Path
+
+import yaml
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+LOGGING_FILE = BASE_DIR / "logging.yaml"
+
+with open(LOGGING_FILE, 'r', encoding='utf-8') as file:
+    config = yaml.safe_load(file)
+
+config["handlers"]["file"]["filename"] = str(BASE_DIR / "app.log")
+
+logging.config.dictConfig(config)
+
+logger = logging.getLogger("persistencia_api")
+logger.info("Sistema de logging incializado.")
